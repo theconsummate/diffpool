453963ea7874618223441454f1fcaac01dd0bdf2
Dhruv Mishra <ubiquitiousdm@gmail.com>
2019-07-24 00:39:10 +0200

    adding viz file

---
+++ viz.py
@@ -0,0 +1,51 @@
+import os
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+def visualise3D(verts,angle=0,view=0,figsize=20):
+# Takes as input a list of coords, where each coord is a list that goes [x,y,z].
+    if type(verts[0]) == str:
+        verts = [[x.split(' ')[1],x.split(' ')[2],x.split(' ')[3].split('\n')[0]] for x in verts]
+        xs = [float(x[0]) for x in verts]
+        ys = [float(x[1]) for x in verts]
+        zs = [float(x[2]) for x in verts]
+    elif type(verts)==list:
+        xs = [x[0] for x in verts]
+        ys = [x[1] for x in verts]
+        zs = [x[2] for x in verts]
+    else:
+        xs = verts[:,0]
+        ys = verts[:,1]
+        zs = verts[:,2]
+
+    fig = plt.figure(figsize=(figsize,figsize))
+    ax = fig.add_subplot(111, projection='3d')
+    #angle+=45
+    ax.view_init(view, angle)
+
+    ax.scatter(xs, ys, zs, c='b', marker='+')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    # plt.show()
+    plt.savefig('clusters.png')
+
+def read_mesh_input(datadir="data", dataname="mesh"):
+    train_file = os.path.join(datadir, dataname, 'Ytrn.npz')
+    '''this edges file is constant and therefore it can be used to create
+    a graph structure once. Make copies of this graph object and set the
+    node features as the point coordinates for different input graphs.
+    '''
+    train_np_file = np.load(train_file)
+    train_points  = train_np_file[train_np_file.files[0]][:10,:,:]
+    shape = train_points.shape
+    print(shape)
+    visualise3D(train_points.reshape(shape[0] * shape[1], shape[2]))
+    # print(train_points[:,:,])
+
+
+if __name__ == "__main__":
+    read_mesh_input()
