e0e87511756662732381d37c43a58b918b004710
Dhruv Mishra <ubiquitiousdm@gmail.com>
2019-07-24 12:09:04 +0200

    added color by assignment

--- viz.py
+++ viz.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-def visualise3D(verts,angle=0,view=0,figsize=20):
+def visualise3D(verts,assignment,angle=0,view=0,figsize=20):
 # Takes as input a list of coords, where each coord is a list that goes [x,y,z].
     if type(verts[0]) == str:
         verts = [[x.split(' ')[1],x.split(' ')[2],x.split(' ')[3].split('\n')[0]] for x in verts]
@@ -25,7 +25,7 @@ def visualise3D(verts,angle=0,view=0,figsize=20):
     #angle+=45
     ax.view_init(view, angle)
 
-    ax.scatter(xs, ys, zs, c='b', marker='+')
+    ax.scatter(xs, ys, zs, c=assignment, marker='+')
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     ax.set_zlabel('Z Label')
@@ -43,7 +43,9 @@ def read_mesh_input(datadir="data", dataname="mesh"):
     train_points  = train_np_file[train_np_file.files[0]][:10,:,:]
     shape = train_points.shape
     print(shape)
-    visualise3D(train_points.reshape(shape[0] * shape[1], shape[2]))
+    num_points = shape[0] * shape[1]
+    assignment = np.random.randint(2, size=num_points)
+    visualise3D(train_points.reshape(num_points, shape[2]), assignment)
     # print(train_points[:,:,])
 
 
